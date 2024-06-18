3032155c4f373fbd24fe1f3866fd3059ec7f0109
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-18 23:49:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,19 +23,19 @@ class Dog extends Animal {
 	}
 }
 ///
-const animal = new Animal("Unknown");
-console.log(animal.species); 
-animal.makeSound(); 
+// const animal = new Animal("Unknown");
+// console.log(animal.species); 
+// animal.makeSound(); 
 
-const cat = new Cat("Cat");
-console.log(cat.species); 
-cat.makeSound(); 
-cat.purr();
+// const cat = new Cat("Cat");
+// console.log(cat.species); 
+// cat.makeSound(); 
+// cat.purr();
 
-const dog = new Dog("Dog");
-console.log(dog.species); 
-dog.makeSound(); 
-dog.bark(); 
+// const dog = new Dog("Dog");
+// console.log(dog.species); 
+// dog.makeSound(); 
+// dog.bark(); 
 
 // Do not change the code below this line
 window.Animal = Animal;
